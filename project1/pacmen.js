6086b572d9bb1c33f9df09f230f9b523a591616c
snando2020 <snando2011@gmail.com>
2022-06-04 10:36:06 +0200

    master

--- project1/pacmen.js
+++ project1/pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-    ['project1/images/PacMan1.png', 'project1/images/PacMan2.png'],
-    ['project1/images/PacMan3.png', 'project1/images/PacMan4.png'],
+    ['../project1/images/PacMan1.png', '../project1/images/PacMan2.png'],
+    ['../project1/images/PacMan3.png', '../project1/images/PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -24,7 +24,7 @@ function makePac() {
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
-    newimg.src = 'project1/images/PacMan1.png';
+    newimg.src = '../project1/images/PacMan1.png';
     newimg.width = 100;
 
     // TODO: set position here
