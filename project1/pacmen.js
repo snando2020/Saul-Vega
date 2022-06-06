151ca20532ebaf17b116c535c49b57ba8f9d8962
snando2020 <snando2011@gmail.com>
2022-06-06 23:44:17 +0200

    master

--- project1/pacmen.js
+++ project1/pacmen.js
@@ -3,12 +3,9 @@ const pacArray = [
     ['./project1/images/PacMan1.png', './project1/images/PacMan2.png'],
     ['./project1/images/PacMan3.png', './project1/images/PacMan4.png']
 ];
-
 var focus = 0;
 var direction = 0;
-
 const pacMen = []; // This array holds all the pacmen        
-
 function setToRandom(scale) {
     return {
         x: Math.random() * scale,
@@ -24,7 +21,6 @@ function makePac() {
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
-
     newimg.src = './project1/images/PacMan1.png';
     newimg.width = 100;
     //
@@ -41,15 +37,15 @@ function makePac() {
         velocity,
         newimg,
         direction,
-        focus
+        focuse
     }
 }
 
 function update() {
     //loop over pacmen array and move each one and move image in DOM
     pacMen.forEach((item) => {
-        item.focus = (item.focus + 1) % 2;
-        item.newimg.src = pacArray[item.direction][item.focus];
+        item.focuse = (item.focuse + 1) % 2;
+        item.newimg.src = pacArray[item.direction][item.focuse];
         checkCollisions(item)
         item.position.x += item.velocity.x;
         item.position.y += item.velocity.y;
