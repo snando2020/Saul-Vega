9fe18ca146dac60c5636c969ecb0f633b2c30a58
snando2020 <snando2011@gmail.com>
2022-06-07 03:20:22 +0200

    master

--- project2/eyes.js
+++ project2/eyes.js
@@ -3,9 +3,10 @@ document.onmousemove = (event) => {
     const x = (event.clientX * 100) / window.innerWidth + '%';
     const y = (event.clientY * 100) / window.innerHeight + '%';
 
-    for (let i = 0; i < 2; i++) {
-        balls[i].style.left = x + 'px';
-        balls[i].style.top = y + 'px';
-        balls[i].transform = `translate(-${x},-${y})`;
-    }
-};
+    balls1.style.left = x + 'px';
+    balls1.style.top = y + 'px';
+    balls1.transform = `translate(-${x},-${y})`;
+    balls2.style.left = x + 'px';
+    balls2.style.top = y + 'px';
+    balls2.transform = `translate(-${x},-${y})`;
+}
